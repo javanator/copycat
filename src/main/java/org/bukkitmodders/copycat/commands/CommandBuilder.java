26755ac879f6770e21dd989daf736d5908a82f97
Nathan Roy <nroy.nathan@gmail.com>
2025-09-28 17:49:06 -0700

    Fix thread scheduling

--- src/main/java/org/bukkitmodders/copycat/commands/CommandBuilder.java
+++ src/main/java/org/bukkitmodders/copycat/commands/CommandBuilder.java
@@ -15,10 +15,13 @@ import lombok.SneakyThrows;
 import okhttp3.*;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkitmodders.copycat.Application;
 import org.bukkitmodders.copycat.managers.PlayerSettingsManager;
 import org.bukkitmodders.copycat.model.PlayerSettingsType;
+import org.bukkitmodders.copycat.model.RevertibleBlock;
 import org.bukkitmodders.copycat.services.Canvas;
 import org.jetbrains.annotations.NotNull;
 
@@ -27,11 +30,18 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.Stack;
+import java.util.concurrent.LinkedBlockingDeque;
 
 
-public class CommandBuilders {
+public class CommandBuilder {
 
-    private OkHttpClient httpClient = new OkHttpClient.Builder().followRedirects(true).build();
+    private final OkHttpClient httpClient = new OkHttpClient.Builder().followRedirects(true).build();
+    private final Application application;
+
+    public CommandBuilder(Application application) {
+        this.application = application;
+    }
 
     public LiteralArgumentBuilder<CommandSourceStack> buildAdminCommand() {
         return Commands.literal("admin")
@@ -49,7 +59,7 @@ public class CommandBuilders {
         return Commands.literal("list")
                 .executes(context -> {
                     String player = context.getSource().getSender().getName();
-                    PlayerSettingsManager playerSettings = Application.getInstance().getConfigurationManager().getPlayerSettings(player);
+                    PlayerSettingsManager playerSettings = application.getConfigurationManager().getPlayerSettings(player);
                     context.getSource().getSender().sendMessage("Copycat Image List:");
                     playerSettings.getShortcuts().forEach(s -> {
                         context.getSource().getSender().sendMessage(s.getName() + " URL: " + s.getUrl());
@@ -66,7 +76,7 @@ public class CommandBuilders {
                     String name = commandContext.getArgument("name", String.class);
                     String url = commandContext.getArgument("url", String.class);
                     String playerName = commandContext.getSource().getSender().getName();
-                    PlayerSettingsManager playerSettings = Application.getInstance().getConfigurationManager().getPlayerSettings(playerName);
+                    PlayerSettingsManager playerSettings = application.getConfigurationManager().getPlayerSettings(playerName);
                     playerSettings.addShortcut(name, url);
                     return Command.SINGLE_SUCCESS;
                 });
@@ -77,7 +87,7 @@ public class CommandBuilders {
                 .then(Commands.argument("name", StringArgumentType.string())
                         .suggests((commandContext, suggestionsBuilder) -> {
                             String player = commandContext.getSource().getSender().getName();
-                            PlayerSettingsManager playerSettings = Application.getInstance().getConfigurationManager().getPlayerSettings(player);
+                            PlayerSettingsManager playerSettings = application.getConfigurationManager().getPlayerSettings(player);
                             // Get the partial input to filter suggestions
                             String input = suggestionsBuilder.getInput();
                             String[] parts = input.split(" ");
@@ -99,21 +109,38 @@ public class CommandBuilders {
     public LiteralArgumentBuilder<CommandSourceStack> buildCopyCommand() {
         return Commands.literal("copy")
                 .then(Commands.argument("shortcut", StringArgumentType.string())
+                        .suggests((commandContext, suggestionsBuilder) -> {
+                            String player = commandContext.getSource().getSender().getName();
+                            PlayerSettingsManager playerSettings = application.getConfigurationManager().getPlayerSettings(player);
+                            // Get the partial input to filter suggestions
+                            String input = suggestionsBuilder.getInput();
+                            String[] parts = input.split(" ");
+                            String partial = parts.length > 0 ? parts[parts.length - 1] : "";
+                            // Add matching shortcuts as suggestions
+                            if (playerSettings.getShortcuts() != null) {
+                                playerSettings.getShortcuts().stream()
+                                        .map(shortcut -> shortcut.getName())
+                                        .filter(name -> name.toLowerCase().startsWith(partial.toLowerCase()))
+                                        .forEach(name -> suggestionsBuilder.suggest(name));
+                            }
+                            return suggestionsBuilder.buildFuture();
+                        })
                         .executes(context -> {
                             String shortcutName = context.getArgument("shortcut", String.class);
-                            PlayerSettingsManager playerSettings = Application.getInstance().getConfigurationManager().getPlayerSettings(context.getSource().getSender().getName());
+                            Player player = (Player) context.getSource().getSender();
+                            PlayerSettingsManager playerSettings = application.getConfigurationManager().getPlayerSettings(player.getName());
                             PlayerSettingsType.Shortcut foundShortcut = playerSettings.getShortcuts().stream()
                                     .filter(s -> s.getName().equalsIgnoreCase(shortcutName))
                                     .findFirst()
                                     .orElse(null);
                             if (foundShortcut != null) {
-                                context.getSource().getSender().sendMessage("URL for " + shortcutName + ": " + foundShortcut.getUrl());
+                                player.sendMessage("URL for " + shortcutName + ": " + foundShortcut.getUrl());
                                 Request request = new Request.Builder().url(foundShortcut.getUrl()).build();
                                 httpClient.newCall(request).enqueue(new Callback() {
                                     @Override
                                     public void onFailure(@NotNull Call call, @NotNull IOException e) {
                                         //Fail
-                                        context.getSource().getSender().sendMessage("Failed to download " + foundShortcut.getUrl());
+                                        player.sendMessage("Failed to download " + foundShortcut.getUrl());
                                     }
 
                                     @SneakyThrows
@@ -121,21 +148,47 @@ public class CommandBuilders {
                                     public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                                         //Render
                                         BufferedImage image = ImageIO.read(response.body().byteStream());
-                                        final PlayerProfileListResolver profilesResolver = context.getArgument("profile", PlayerProfileListResolver.class);
-                                        final Collection<PlayerProfile> foundProfiles = profilesResolver.resolve(context.getSource());
-                                        Player player = (Player) context.getSource().getSender();
+
                                         Block b = player.getTargetBlock(null, 100);
                                         Location location = new Location(b.getWorld(), b.getX(), b.getY(), b.getZ());
                                         location.setYaw(player.getLocation().getYaw());
                                         location.setPitch(player.getLocation().getPitch());
 
-                                        Canvas canvas = new Canvas(image, context.getSource().getSender(), location);
-                                        canvas.performDraw();
+                                        Canvas canvas = new Canvas(image, player, location);
+
+                                        BukkitScheduler scheduler = application.getServer().getScheduler();
+                                        scheduler.runTask(application, new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                canvas.performDraw();
+                                            }
+                                        });
                                     }
                                 });
                             }
                             return Command.SINGLE_SUCCESS;
                         }));
+    }
+
+    public LiteralArgumentBuilder<CommandSourceStack> buildUndoCommand() {
+        return Commands.literal("undo")
+                .executes(ctx -> {
+                    Player player = (Player) ctx.getSource().getSender();
+                    ctx.getSource().getSender().sendMessage("Undo for " + player.getName());
+
+                    PlayerSettingsManager playerSettings = application.getConfigurationManager().getPlayerSettings(player.getName());
+                    LinkedBlockingDeque<Stack<RevertibleBlock>> undoBuffer = playerSettings.getUndoBuffer();
+
+                    if (!undoBuffer.isEmpty()) {
+                        Stack<RevertibleBlock> lastImageBlocks = undoBuffer.pop();
+
+                        while (!lastImageBlocks.isEmpty()) {
+                            lastImageBlocks.pop().revert();
+                        }
+                    }
+
+                    return Command.SINGLE_SUCCESS;
+                });
     }
 
     public LiteralArgumentBuilder<CommandSourceStack> buildPollCommand() {
@@ -151,7 +204,7 @@ public class CommandBuilders {
                         .executes(context -> {
                             boolean ditheringEnabled = context.getArgument("enabled", Boolean.class);
                             String player = context.getSource().getSender().getName();
-                            PlayerSettingsManager playerSettings = Application.getInstance().getConfigurationManager().getPlayerSettings(player);
+                            PlayerSettingsManager playerSettings = application.getConfigurationManager().getPlayerSettings(player);
                             playerSettings.setDithering(ditheringEnabled);
                             return Command.SINGLE_SUCCESS;
                         })))
@@ -162,7 +215,7 @@ public class CommandBuilders {
                                             Integer width = ctx.getArgument("width", Integer.class);
                                             Integer height = ctx.getArgument("height", Integer.class);
                                             String player = ctx.getSource().getSender().getName();
-                                            PlayerSettingsManager playerSettings = Application.getInstance().getConfigurationManager().getPlayerSettings(player);
+                                            PlayerSettingsManager playerSettings = application.getConfigurationManager().getPlayerSettings(player);
                                             playerSettings.setBuildDimensions(width, height);
                                             return Command.SINGLE_SUCCESS;
                                         }))))
@@ -173,6 +226,4 @@ public class CommandBuilders {
                                     return suggestionsBuilder.buildFuture();
                                 })));
     }
-
-
 }
